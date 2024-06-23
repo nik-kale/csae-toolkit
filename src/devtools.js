c45291f54b21931ce50bb6049d6d528031a58fee
Nik Kale <nik.kale12@gmail.com>
2024-06-22 19:31:10 -0700

    Phase3: Sidepanel version migrated final 3

--- src/devtools.js
+++ src/devtools.js
@@ -8,13 +8,3 @@ chrome.devtools.panels.create(
     console.log("CSAE Toolkit Storage panel created");
   }
 );
-
-chrome.devtools.panels.create(
-  "Storage Area Viewer",
-  "icons/icon128.png",
-  "sa-panel.html",
-  function(panel) {
-    console.log("Storage Area Viewer panel created");
-  }
-);
-
